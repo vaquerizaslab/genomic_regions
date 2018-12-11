77d830ffb1330fb0a73eb17f1ce224fd6214636e
kai <kai.kruse@mpi-muenster.mpg.de>
2018-12-11 10:46:54 +0100

    Annotate helpers module

--- genomic_regions/helpers.py
+++ genomic_regions/helpers.py
@@ -1,3 +1,7 @@
+"""
+Helper functions for working with genomic regions.
+"""
+
 import os
 import re
 import numpy as np
@@ -6,6 +10,7 @@ import numpy as np
 def human_format(num, precision=0):
     """
     Format a number as a string, suffixing letter for 1000 (K), 100000 (M), ...
+
     :param num: any number larger than zero
     :param precision: number of positions after decimal point
     :return: string representing the number
@@ -19,6 +24,14 @@ def human_format(num, precision=0):
 
 
 def str_to_int(num_string, decimal_separator='.', thousand_separator=','):
+    """
+    Convert a string denoting a genomic location to int (base pairs).
+
+    :param num_string: input :class:`~str`
+    :param decimal_separator: Decimal separator used for float conversion
+    :param thousand_separator: Thousand separator (to be ignored)
+    :return: int (base pairs)
+    """
     num_string = num_string.replace(thousand_separator, '').lower()
     try:
         return int(num_string)
@@ -47,59 +60,18 @@ def str_to_int(num_string, decimal_separator='.', thousand_separator=','):
             raise ValueError("Cannot convert '{}' to integer!".format(num_string))
 
 
+def _convert(text):
+    return int(text) if text.isdigit() else text.lower()
+
+
 def natural_sort(l):
-    def convert(text):
-        return int(text) if text.isdigit() else text.lower()
-    return sorted(l, key=lambda key: [convert(c) for c in re.split('([0-9]+)', key)])
+    """
+    Sort list of chromosome names "naturally"
 
-
-def natural_cmp(pa, pb):
-    i = 0
-    j = 0
-
-    if pa[-2] == '/' == pb[-2]:
-        pa = pa[:-2]
-        pb = pb[:-2]
-
-    try:
-        while i < len(pa) and j < len(pb):
-            if pa[i].isdigit() and pb[j].isdigit():
-                # digit comparison
-                while pa[i] == '0':
-                    i += 1
-                while pb[j] == '0':
-                    j += 1
-                while pa[i].isdigit() and pb[j].isdigit() and pa[i] == pb[j]:
-                    i += 1
-                    j += 1
-                if pa[i].isdigit() and pb[j].isdigit():
-                    k = 0
-                    try:
-                        while pa[i+k].isdigit() and pb[j+k].isdigit():
-                            k += 1
-                    except IndexError:
-                        if i+k < len(pa):
-                            return 1
-                        if j+k < len(pb):
-                            return -1
-                        # both ran over index
-                        return int(pa[i]) - int(pb[j])
-                    return 1 if pa[i+k].isdigit() else -1 if pb[j+k].isdigit() else int(pa[i]) - int(pb[j])
-                elif pa[i].isdigit():
-                    return 1
-                elif pb[j].isdigit():
-                    return -1
-                elif i != j:
-                    return 1 if i < j else -1
-            else:
-                # string comparison
-                if pa[i] != pb[j]:
-                    return -1 if pa[i] < pb[j] else 1
-                i += 1
-                j += 1
-    except IndexError:
-        pass
-    return 1 if i < len(pa) else -1 if j < len(pb) else 0
+    :param l: :class:`~list` of strings
+    :return: naturally sorted :class:`~list` of strings
+    """
+    return sorted(l, key=lambda key: [_convert(c) for c in re.split('([0-9]+)', key)])
 
 
 def apply_sliding_func(a, window, func=np.ma.mean):
@@ -121,6 +93,14 @@ def apply_sliding_func(a, window, func=np.ma.mean):
 
 
 def intervals_weighted_mean(intervals):
+    """
+    Weighted mean for merging multiple interval scores into one.
+
+    Takes into account the size of each interval as score weight.
+
+    :param intervals: list of :class:`~tuple` (start, end, score)
+    :return: float score
+    """
     intervals = np.array(intervals)
     if len(intervals) == 0:
         return np.nan
@@ -133,26 +113,3 @@ def intervals_weighted_mean(intervals):
     # safety
     weights = [weight if weight > 0 else 1 for weight in weights]
     return np.average(valid[:, 2], weights=weights)
-
-
-def which(program):
-    """
-    Check if executable exists in PATH
-    :param program: executable name or path to executable
-    :return: full path if found, None if not
-    """
-    def is_exe(fpath):
-        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
-
-    fpath, fname = os.path.split(program)
-    if fpath:
-        if is_exe(program):
-            return program
-    else:
-        for path in os.environ["PATH"].split(os.pathsep):
-            path = path.strip('"')
-            exe_file = os.path.join(path, program)
-            if is_exe(exe_file):
-                return exe_file
-
-    return None
